438d035daf8cac6dece83c4066794f894709ed70
David Leigh <daleighan@gmail.com>
2018-12-14 02:53:02 -0800

    The context is now passed to children with breadth-first search

--- src/elementFactory.js
+++ src/elementFactory.js
@@ -1,4 +1,4 @@
-const elementFactory = (name, HTML, store, functions) => {
+const elementFactory = (name, HTML, store, functions, eventListeners) => {
   if (!name.match('-')) {
     throw 'Invalid custom element name. Each custom element name must contain at list one "-" character'
   }
@@ -9,6 +9,7 @@ const elementFactory = (name, HTML, store, functions) => {
         constructor() {
           super();
           this.store = store;
+          this.attachShadow({mode: 'open'});
           Object.assign(this, functions);
         }
         getStore() {
@@ -20,7 +21,13 @@ const elementFactory = (name, HTML, store, functions) => {
     let child = new DOMParser().parseFromString(HTML, 'text/html').body
       .firstChild;
     child.context = newElement;
-    newElement.appendChild(child);
+    let queue = [child];
+    while (queue.length) {
+      let current = queue.shift();
+      current.context = newElement;
+      queue = current.children ? queue.concat(Array.from(current.children)) : queue;
+    }
+    newElement.shadowRoot.appendChild(child);
     return newElement;
   } else {
     throw "Element name is already taken. Please use a unique identifier for each new element."
